a6d6ac2be45976f8884c98798dd7de7b21c532e6
Rishon73 <sshiff@gmail.com>
2018-02-28 09:16:20 -0500

    Close browser only when local

--- src/main/java/com/mf/LeanFtTest.java
+++ src/main/java/com/mf/LeanFtTest.java
@@ -31,37 +31,43 @@ public class LeanFtTest extends UnitTestClassBase {
     public void afterMethod() throws Exception {
     }
 
+/*
     @Test
     public void SRFMobileBrowser() throws Exception {
         BrowserBuilder test = new BrowserBuilder(BrowserType.CHROME, "ZX1G22D3VD", "Shiff's remote SRF mobile browser test");
         mcutils.logMessages("SRF mobile browser test", LOG_LEVEL.INFO);
-        runMyTest(test.getBrowser());
+        runMyTest(test.getBrowser(), false);
     }
 
     @Test //(threadPoolSize = 3, invocationCount = 1)
     public void SRFBrowser() throws Exception {
         BrowserBuilder test = new BrowserBuilder(BrowserType.CHROME, "63", "Windows", "10", "Shiff's remote browser test");
         mcutils.logMessages("SRF browser test", LOG_LEVEL.INFO);
-        runMyTest(test.getBrowser());
+        runMyTest(test.getBrowser(), false);
     }
+*/
 
     @Test
     public void localBrowser() throws Exception {
         BrowserBuilder test = new BrowserBuilder(BrowserType.CHROME);
         mcutils.logMessages("Local browser test", LOG_LEVEL.INFO);
-        runMyTest(test.getBrowser());
+        runMyTest(test.getBrowser(), true);
     }
 
     // Execute the actual test
-    private void runMyTest(Browser browser) throws Exception {
+    private void runMyTest(Browser browser, boolean closeBrowser) throws Exception {
         mcutils.logMessages("Navigate to...", LOG_LEVEL.INFO);
         browser.navigate("http://tinyurl.com/hpe-shop");
 
         mcutils.logMessages("waiting 10 seconds", LOG_LEVEL.INFO);
         mcutils.windowSync(10000);
 
-        mcutils.logMessages("Closing browser", LOG_LEVEL.INFO);
-        browser.close();
+        
+
+        if (closeBrowser) {
+            mcutils.logMessages("Closing browser", LOG_LEVEL.INFO);
+            browser.close();
+        }
 
     }
 }
